30cc3d535b434a2cc6e8eda79a444e9ab93d5dd5
김승현 <poia689@naver.com>
2021-08-15 21:07:36 +0900

    8/15 반응형 작업
    profile 반응형 작업

--- Portfolio/WebContent/js/main.js
+++ Portfolio/WebContent/js/main.js
@@ -25,3 +25,27 @@ $(window).scroll(function(){
 			$('.menuBtn').removeClass('show');
 		}
 });
+
+/**
+ * 반응형
+ */
+
+$(window).resize(function (){
+	  // width값을 가져오기
+	  var width_size = window.outerWidth;
+	  
+	  // 740 이하인지 if문으로 확인
+	  if (width_size <= 740) {
+		  
+		  if($('#br').length == 0) { // br 클래스 존재 여부 확인
+			  $('.institution').after('<br id="br">');
+		  }
+		  
+	  } else { // 740 아하가 아니라면 해당 클래스 삭제
+		  
+		 if($('#br').length == 1) {
+			 $('#br').remove();
+		 }
+	  }
+	});
+
